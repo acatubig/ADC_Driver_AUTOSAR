27d2916d3d9aaac53ecf512f8a63ccac3cd061a5
Acatubig <acatubig45@gmail.com>
2024-11-15 20:29:02 +0800

    Debug RAM files

---
+++ Debug_RAM/Project_Settings/Startup_Code/system_S32K144.d
@@ -0,0 +1,20 @@
+Project_Settings/Startup_Code/system_S32K144.o: \
+ ../Project_Settings/Startup_Code/system_S32K144.c \
+ D:/ADCDriver_Project/include/device_registers.h \
+ D:/ADCDriver_Project/include/s32_core_cm4.h \
+ D:/ADCDriver_Project/include/S32K144.h \
+ D:/ADCDriver_Project/include/S32K144_features.h \
+ D:/ADCDriver_Project/include/devassert.h \
+ D:/ADCDriver_Project/include/system_S32K144.h
+
+D:/ADCDriver_Project/include/device_registers.h:
+
+D:/ADCDriver_Project/include/s32_core_cm4.h:
+
+D:/ADCDriver_Project/include/S32K144.h:
+
+D:/ADCDriver_Project/include/S32K144_features.h:
+
+D:/ADCDriver_Project/include/devassert.h:
+
+D:/ADCDriver_Project/include/system_S32K144.h:
