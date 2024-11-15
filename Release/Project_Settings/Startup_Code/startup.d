87ccfe78d187f0696912e55ecd40ed801be97eb2
Acatubig <acatubig45@gmail.com>
2024-11-15 20:17:31 +0800

    added files via upload

---
+++ Release/Project_Settings/Startup_Code/startup.d
@@ -0,0 +1,20 @@
+Project_Settings/Startup_Code/startup.o: \
+ ../Project_Settings/Startup_Code/startup.c \
+ D:/ADCDriver_Project/include/startup.h \
+ D:/ADCDriver_Project/include/device_registers.h \
+ D:/ADCDriver_Project/include/s32_core_cm4.h \
+ D:/ADCDriver_Project/include/S32K144.h \
+ D:/ADCDriver_Project/include/S32K144_features.h \
+ D:/ADCDriver_Project/include/devassert.h
+
+D:/ADCDriver_Project/include/startup.h:
+
+D:/ADCDriver_Project/include/device_registers.h:
+
+D:/ADCDriver_Project/include/s32_core_cm4.h:
+
+D:/ADCDriver_Project/include/S32K144.h:
+
+D:/ADCDriver_Project/include/S32K144_features.h:
+
+D:/ADCDriver_Project/include/devassert.h:
